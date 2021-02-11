83a4d668f913bee0ff983ed7b1d6041e8a9835f7
Eric Leyder <47430289+leydere@users.noreply.github.com>
2021-02-11 12:29:28 -0500

    Added time and date picker to ActivityAddIrritant

---
+++ app/src/main/java/com/leydere/irrsymptrack/ActivityAddIrritant.java
@@ -0,0 +1,101 @@
+package com.leydere.irrsymptrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class ActivityAddIrritant extends AppCompatActivity {
+    private static Button date, time;
+    private static TextView set_date, set_time;
+    private static final int Date_id = 0;
+    private static final int Time_id = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        date = (Button) findViewById(R.id.selectdate);
+        time = (Button) findViewById(R.id.selecttime);
+        set_date = (TextView) findViewById(R.id.set_date);
+        set_time = (TextView) findViewById(R.id.set_time);
+        date.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Show Date dialog
+                showDialog(Date_id);
+            }
+        });
+        time.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Show time dialog
+                showDialog(Time_id);
+            }
+        });
+
+    }
+
+    protected Dialog onCreateDialog(int id) {
+
+        // Get the calander
+        Calendar c = Calendar.getInstance();
+
+        // From calander get the year, month, day, hour, minute
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        switch (id) {
+            case Date_id:
+
+                // Open the datepicker dialog
+                return new DatePickerDialog(ActivityAddIrritant.this, date_listener, year,
+                        month, day);
+            case Time_id:
+
+                // Open the timepicker dialog
+                return new TimePickerDialog(ActivityAddIrritant.this, time_listener, hour,
+                        minute, false);
+
+        }
+        return null;
+    }
+
+    // Date picker dialog
+    DatePickerDialog.OnDateSetListener date_listener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // store the data in one string and set it to text
+            String date1 = String.valueOf(month) + "/" + String.valueOf(day)
+                    + "/" + String.valueOf(year);
+            set_date.setText(date1);
+        }
+    };
+    TimePickerDialog.OnTimeSetListener time_listener = new TimePickerDialog.OnTimeSetListener() {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hour, int minute) {
+            // store the data in one string and set it to text
+            String time1 = String.valueOf(hour) + ":" + String.valueOf(minute);
+            set_time.setText(time1);
+        }
+    };
+}
