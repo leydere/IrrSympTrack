a14c1fb8b3dd31308dc1193478b038a59653e889
Eric Leyder <47430289+leydere@users.noreply.github.com>
2021-03-08 16:13:16 -0500

    Added graph to xml

--- app/src/main/java/com/leydere/irrsymptrack/ActivityGraphView.java
+++ app/src/main/java/com/leydere/irrsymptrack/ActivityGraphView.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.jjoe64.graphview.GraphView;
+
 public class ActivityGraphView extends AppCompatActivity {
 
+    GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_view);
 
+        graph.findViewById(R.id.graph);
 
     }
 }
