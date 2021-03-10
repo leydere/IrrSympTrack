54b623d0669e8ab40f68b301800abdcbc1947854
Eric Leyder <47430289+leydere@users.noreply.github.com>
2021-03-10 16:29:12 -0500

    DB data to graph
    Created function that populates graph with severity and date data from the symptoms graph.  Current format of date as integer is largely useless and unreadable on graph.  Graph axis are not labeled.

--- app/src/main/java/com/leydere/irrsymptrack/ActivityGraphView.java
+++ app/src/main/java/com/leydere/irrsymptrack/ActivityGraphView.java
@@ -1,6 +1,8 @@
 package com.leydere.irrsymptrack;
 
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,10 +10,19 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class ActivityGraphView extends AppCompatActivity {
 
     LineGraphSeries<DataPoint> series;
     GraphView graph;
+    Calendar calendar;
+
+    ArrayList<ModelSymptom> allSymptomsList;
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +31,14 @@ public class ActivityGraphView extends AppCompatActivity {
 
         graph = findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
+        calendar = Calendar.getInstance();
 
-        series = createLinearSeries();
+        databaseHelper = new DatabaseHelper(ActivityGraphView.this);
+        allSymptomsList = getAllSymptoms();
+        //Toast.makeText(ActivityGraphView.this, "Title of second symptom = " + allSymptomsList.get(1).getSymTitle(), Toast.LENGTH_SHORT).show(); //WORKS!!
+
+        //series = createLinearSeries();
+        series = testLineSeries1();
 
         graph.addSeries(series);
 
@@ -33,12 +50,35 @@ public class ActivityGraphView extends AppCompatActivity {
         x = 0;
         series = new LineGraphSeries<DataPoint>();
 
+        //Let's try going through each DB item one by one, setting the data to ints, and appending to series.
+        //In the final version I am likely to do some of the date grouping on the DB side.
 
+        for (int i = 0; i < allSymptomsList.size(); i++) {
+            ModelSymptom modelSymptom = allSymptomsList.get(i);
 
+            //y = sev
+            int severity = 1 + Integer.valueOf(modelSymptom.getSymSeverity());
+            y = severity;
+            //x = date
+            String date = modelSymptom.getSymTimeDate();
+            //set time data to calendar - took this from the load existing record to add symptom activity
+            SimpleDateFormat dbStringToCalendar = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.sss", Locale.ENGLISH);
+            try{
+                calendar.setTime(dbStringToCalendar.parse(date));
+                //Toast.makeText(ActivityGraphView.this, "HERE!", Toast.LENGTH_SHORT).show(); //resolved reaching here with Calendar get instance
+                CharSequence dateCharSequence = DateFormat.format("yyyyMMdd", calendar);
+                int formattedDate = Integer.parseInt(dateCharSequence.toString());
+                x = formattedDate;
+            } catch (Exception e) {
+                Toast.makeText(ActivityGraphView.this, "input error from calendar", Toast.LENGTH_SHORT).show();
+            }
+            series.appendData(new DataPoint(x, y), true, allSymptomsList.size());
+        }
         return series;
     }
 
-    public LineGraphSeries<DataPoint> createLinearSeries(){
+    //First graph series function.  Testing graph population using linear function.
+    private LineGraphSeries<DataPoint> createLinearSeries(){
         int y,x;
         x = 0;
         series = new LineGraphSeries<DataPoint>();
@@ -50,5 +90,11 @@ public class ActivityGraphView extends AppCompatActivity {
             series.appendData(new DataPoint(x, y), true, 5); //Youtuber says maxDataPoints attribute must equal number of data points in series
         }
         return series;
+    }
+
+    private ArrayList<ModelSymptom> getAllSymptoms(){
+        ArrayList<ModelSymptom> arrayListToReturn = new ArrayList<>();
+        arrayListToReturn.addAll(databaseHelper.getAllSymptoms());
+        return arrayListToReturn;
     }
 }
