4106758f3d5cb4e024564f01a86b7a7072ea1a0e
Eric Leyder <47430289+leydere@users.noreply.github.com>
2021-03-24 22:18:44 -0400

    Started exploring the fragment navigation from other activities again.
    Solutions to this dilemma appear to be depreciated and not fully compatible with what I have coded so far.  Undid most of what I did.  Commented out a couple spots possibly worth keeping.  Kept some of the cleanup I implemented.

--- app/src/main/java/com/leydere/irrsymptrack/MainActivity.java
+++ app/src/main/java/com/leydere/irrsymptrack/MainActivity.java
@@ -3,24 +3,17 @@ package com.leydere.irrsymptrack;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean goToFragment2;
     Button buttonNavToGraphView;
 
     @Override
@@ -30,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button buttonNavToGraphView = findViewById(R.id.buttonNavToGraphView);
+        buttonNavToGraphView = findViewById(R.id.buttonNavToGraphView);
 
         buttonNavToGraphView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,49 +33,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        /*
-        //Trying to get intent from add symptom activity to navigate to fragment 2 when loaded
-        try {
-            Intent intent = getIntent();
-            goToFragment2 = intent.getBooleanExtra("goToFragment2", false);
-            Toast.makeText(MainActivity.this, "Intent Extra = " + goToFragment2, Toast.LENGTH_SHORT).show();
-            //Navigation.findNavController(FragmentFirst.this).navigate(R.id.action_FirstFragment_to_SecondFragment);
-            Intent i = new Intent(MainActivity.this, FragmentSecond.class);
-            startActivity(i);
-        } catch (Exception e) {
-
-        }*/
-
-        /*
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-                ModelIrritant modelIrritant;
-                try{
-                    modelIrritant = new ModelIrritant("testTitle1", "testTimeDate1", "testSeverity1");
-                }
-                catch (Exception e) {
-                    Toast.makeText(MainActivity.this, "input error toast", Toast.LENGTH_SHORT).show();
-                    modelIrritant = new ModelIrritant("error", "error", "error");
-                }
-
-                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
-                boolean success = databaseHelper.addIrritantRecord(modelIrritant);
-
-                if (success == true) {
-                    Toast.makeText(MainActivity.this, "record added successfully", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "record added failure", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-
-        });*/
 
     }
 
